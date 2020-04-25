045161345d1091552876bb33b4eff5c23c11cf66
Callum Hart <callumhartemail@gmail.com>
2020-04-25 15:59:46 +0100

    apt-get -> apt + full-upgrade

--- setup.sh
+++ setup.sh
@@ -6,11 +6,13 @@ sudo sed -i "s/# en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/g" -i /etc/locale.gen
 sudo locale-gen en_GB.UTF-8
 sudo update-locale en_GB.UTF-8
 
-sudo apt-get update && sudo apt-get upgrade -y
+sudo apt update && sudo apt full-upgrade -y
 
 sudo apt-get install zsh -y
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
+
+chsh -s $(which zsh)
 
 zsh
