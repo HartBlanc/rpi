5c51919327e3a9d823b4fa02a77359af088e5fe5
Callum Hart <callumhartemail@gmail.com>
2020-04-25 15:24:39 +0100

    minor format stuff

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
-sudo sed -i "s/# en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/g" -i /etc/locale.gen
-sudo locale-gen en_GB.UTF-8
-sudo update-locale en_GB.UTF-8
-
 export LANGUAGE=en_GB.UTF-8
 export LANG=en_GB.UTF-8
 export LC_ALL=en_GB.UTF-8
+
+sudo sed -i "s/# en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/g" -i /etc/locale.gen
+sudo locale-gen en_GB.UTF-8
+sudo update-locale en_GB.UTF-8
 
 sudo apt-get update && sudo apt-get upgrade -y
 
