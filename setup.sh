4529f210545f5d46b43a471726233bca540e0bc8
Callum Hart <callumhartemail@gmail.com>
2020-04-25 15:03:17 +0100

    fixed messed up zsh

--- setup.sh
+++ setup.sh
@@ -8,14 +8,6 @@ export LC_ALL=en_GB.UTF-8
 
 sudo apt-get update && sudo apt-get upgrade -y
 
-sudo sed -i "s/# en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/g" -i /etc/locale.gen
-sudo locale-gen en_GB.UTF-8
-sudo update-locale en_GB.UTF-8
-
-export LANGUAGE=en_GB.UTF-8
-export LANG=en_GB.UTF-8
-export LC_ALL=en_GB.UTF-8
-
 sudo apt-get install zsh -y
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
